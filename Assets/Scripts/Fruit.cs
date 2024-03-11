c9ff5b6e19ab3bba11c04e919db217ac5415d130
ReloadingBee <justaschess@gmail.com>
2024-03-11 19:49:38 +0200

    Merging & Fruit Dropping Added

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -1,48 +1,55 @@
+using System;
 using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
-    public int fruitType;
-    Rigidbody2D rb;
-    Game game;
+	public int fruitType;
+	Rigidbody2D rb;
+	Game game;
 
-    [SerializeField] bool isDeleted;
+	[SerializeField] bool isDeleted;
 
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        game = FindObjectOfType<Game>();
-    }
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		game = FindObjectOfType<Game>();
+		isDeleted = false;
+	}
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (!other.gameObject.CompareTag("Fruit")) return;
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (!other.gameObject.CompareTag("Fruit")) return;
 
-        // other IS a fruit
-        var fruit = other.gameObject.GetComponent<Fruit>();
+		// other IS a fruit
+		var fruit = other.gameObject.GetComponent<Fruit>();
 
-        // only merge once before deleting!
-        if (isDeleted || fruit.isDeleted) return;
+		// only merge once before deleting!
+		if (isDeleted || fruit.isDeleted) return;
 
-        if(fruit.fruitType == fruitType)
-        {
-            // fruit types match - combine
-            // check which one's velocity is higher
-            // delete both fruits and spawn another one
-            // in the position of the fruit with a lower velocity
-            var fruitVelocity = fruit.rb.velocity.magnitude;
-            if (fruitVelocity >= rb.velocity.magnitude)
-            {
-                // this is slower than other fruit
-                // spawn new fruit in my position
-                game.SpawnNewFruit(transform.position, fruitType + 1);
-                // only one if statement avoids duplicate fruits from spawning
-                // since only one fruit has a lower velocity
-                isDeleted = true;
-                fruit.isDeleted = true;
-            }
+		if (fruit.fruitType == fruitType)
+		{
+			// fruit types match - combine
+			// check which one's velocity is higher
+			// delete both fruits and spawn another one
+			// in the position of the fruit with a lower velocity
+			var fruitVelocity = fruit.rb.velocity.magnitude;
+			if (fruitVelocity >= rb.velocity.magnitude)
+			{
+				// this is slower than other fruit
+				// spawn new fruit in my position
+				game.SpawnNewFruit(transform.position, fruitType + 1);
+				// only one if statement avoids duplicate fruits from spawning
+				// since only one fruit has a lower velocity
+				isDeleted = true;
+				fruit.isDeleted = true;
+				Destroy(other.gameObject);
+				Destroy(gameObject);
+			}
+		}
+	}
 
-            Destroy(gameObject);
-        }
-    }
+	void Update()
+	{
+		if(isDeleted) Destroy(gameObject);
+	}
 }
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,18 +1,80 @@
 using UnityEngine;
 using System.Collections.Generic;
+using Random = UnityEngine.Random;
 
 public class Game : MonoBehaviour
 {
     [SerializeField] List<GameObject> fruits;
 
-    public void SpawnNewFruit(Vector2 pos, int type)
+    public Fruit SpawnNewFruit(Vector2 pos, int type)
     {
         //the biggest fruit becomes the smallest
         //when the fruit type becomes too large
-        type = type % fruits.Count;
+        type %= fruits.Count;
 
         var fruit = Instantiate(fruits[type], pos, Quaternion.identity).GetComponent<Fruit>();
         fruit.fruitType = type;
-        //print("new fruit");
+        return fruit;
+    }
+
+    public int currentFruit;
+    public int nextFruit;
+    public bool isFalling;
+    public bool isThinking;
+    public bool isGameOver;
+
+    Camera cam;
+
+    void Start()
+    {
+        isGameOver = false;
+        isFalling = false;
+        isThinking = false;
+        currentFruit = Random.Range(0, 3);
+        nextFruit = Random.Range(0, 3);
+        
+        cam = Camera.main;
+    }
+    async void Update()
+    {
+        if (isGameOver) return;
+        if (isFalling) return;
+
+        if (isThinking) return;
+        var hit = Physics2D.Raycast(new Vector2(-2.5f, 3f), Vector2.right, 5f);
+        if (hit && hit.collider.CompareTag("Fruit"))
+        {
+            GameOver();
+            return;
+        }
+        
+        isThinking = true;
+        
+        var fruit = SpawnNewFruit(new Vector2(0, 4), currentFruit);
+        fruit.GetComponent<Rigidbody2D>().gravityScale = 0;
+        
+        await new WaitUntil(()=>Input.GetKeyDown(KeyCode.Mouse0));
+        
+        // Mouse clicked - fall
+        fruit.GetComponent<Rigidbody2D>().gravityScale = 1;
+        
+        // Move fruit to mouse position
+        var x = cam.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, 0)).x;
+        x = Mathf.Clamp(x, -2.5f, 2.5f);
+        fruit.transform.position = new Vector2(x, 4);
+        
+        isFalling = true;
+        currentFruit = nextFruit;
+        nextFruit = Random.Range(0, 3);
+        
+        await new WaitForSeconds(1);
+        
+        isFalling = false;
+        isThinking = false;
+    }
+
+    public void GameOver()
+    {
+        isGameOver = true;
     }
 }
